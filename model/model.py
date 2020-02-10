64fa77457dc8b65467a19e3cdefabbd8b59ccdc2
Cloud <cloudszhang0727@outlook.com>
2020-02-10 15:40:15 +0200

    backup

---
+++ model/model.py
@@ -0,0 +1,89 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import preprocessing , utils
+import os
+import yaml
+from tensorflow.keras.models import load_model
+
+print( tf.VERSION )
+rootPath = './SheldonTrainLargeData/'
+
+q = open(rootPath+"context_sheldon.txt", 'r', encoding='UTF-8')
+questions = q.read()
+a = open(rootPath+"answers_sheldon.txt", 'r', encoding='UTF-8')
+answers = a.read()
+# print(type(questions),"\n",answers)
+all = answers + questions
+answers = [p for p in answers.split('\n')]
+questions = [p for p in questions.split('\n')]
+all_text = [p for p in all.split('\n')]
+print(questions[:2],"\n",answers[:2],"\n",all_text)
+
+# questions = questions[:2000]
+# answers = answers[:2000]
+
+answers_with_tags = list()
+for i in range( len( answers ) ):
+    if type( answers[i] ) == str:
+        answers_with_tags.append( answers[i] )
+    else:
+        questions.pop( i )
+
+answers = list()
+for i in range( len( answers_with_tags ) ) :
+    answers.append( '<START> ' + answers_with_tags[i] + ' <END>' )
+
+tokenizer = preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts( questions + answers )
+VOCAB_SIZE = len( tokenizer.word_index )+1
+print( 'VOCAB SIZE : {}'.format( VOCAB_SIZE ))
+
+print(questions[:5],"\n",answers[:5],"\n",len(questions)==len(answers))
+
+
+# encoder_input_data
+tokenized_questions = tokenizer.texts_to_sequences( questions )
+maxlen_questions = max( [ len(x) for x in tokenized_questions ] )
+
+tokenized_answers = tokenizer.texts_to_sequences( answers )
+maxlen_answers = max( [ len(x) for x in tokenized_answers ] )
+
+tokenized_answers_output = tokenizer.texts_to_sequences( answers )
+for i in range(len(tokenized_answers_output)) :
+    tokenized_answers_output[i] = tokenized_answers_output[i][1:]
+
+
+def str_to_tokens( sentence : str ):
+    words = sentence.lower().split()
+    tokens_list = list()
+    for word in words:
+        tokens_list.append( tokenizer.word_index[ word ] )
+    return preprocessing.sequence.pad_sequences( [tokens_list] , maxlen=maxlen_questions , padding='post')
+
+
+enc_model=load_model(rootPath+ 'modelLarge2enc.h5')
+dec_model=load_model(rootPath+ 'modelLarge2dec.h5')
+
+for _ in range(1):
+    states_values = enc_model.predict(str_to_tokens(input('Enter question : ')))
+    empty_target_seq = np.zeros((1, 1))
+    empty_target_seq[0, 0] = tokenizer.word_index['start']
+    stop_condition = False
+    decoded_translation = ''
+    while not stop_condition:
+        dec_outputs, h, c = dec_model.predict([empty_target_seq] + states_values)
+        sampled_word_index = np.argmax(dec_outputs[0, -1, :])
+        sampled_word = None
+        for word, index in tokenizer.word_index.items():
+            if sampled_word_index == index:
+                decoded_translation += ' {}'.format(word)
+                sampled_word = word
+
+        if sampled_word == 'end' or len(decoded_translation.split()) > maxlen_answers:
+            stop_condition = True
+
+        empty_target_seq = np.zeros((1, 1))
+        empty_target_seq[0, 0] = sampled_word_index
+        states_values = [h, c]
+
+    print(decoded_translation[:-3]) # string
